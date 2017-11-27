9f538f4422c75f3904c443ce4f93f0738d01f521
Unknown <stmihailov@yandex.ru>
2017-11-27 21:50:46 +0300

    правки

--- js/map.js
+++ js/map.js
@@ -15,7 +15,8 @@ function generateRandomNumber(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
-
+var ads = [];
+ads.push({parametres})
 
 for (var i = 1; i <= 8; i++) {
   var locationX = generateRandomNumber(300, 900);
