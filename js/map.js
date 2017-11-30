5207aa27b169fd54be98d15b8fd5a80076748e0d
hramn1 <stmihailov@yandex.ru>
2017-11-30 17:58:09 +0300

    правки

--- js/map.js
+++ js/map.js
@@ -66,18 +66,23 @@ for (var j = 0; j < ads.length; j++) {
 var templateCard = document.querySelector('template').content.querySelector('.map__card');
 var popupTemplate = templateCard.cloneNode(true);
 var mapCard = document.querySelector('.map');
-for (var l = 0; l < ads.length; l++) {
-  var popupAdress = popupTemplate.querySelector('p:first-of-type');
-  popupTemplate.querySelector('h3').textContent = ads[l].offer.title;
-  popupAdress.querySelector('small').textContent = ads[l].offer.address;
-  popupTemplate.querySelector('.popup__price').textContent = ads[l].offer.price + '&#x20bd;/ночь';
-  popupTemplate.querySelector('h4').textContent = ads[l].offer.type;
-  popupTemplate.querySelector('h4 + p').textContent = ads[l].offer.rooms + ' для ' + ads[l].offer.guests + ' гостей';
-  popupTemplate.querySelector('p + p').textContent = 'Заезд после ' + ads[l].offer.checkin + ', выезд до ' + ads[l].offer.checkout;
-  popupTemplate.querySelector('.popup__features').textContent = ads[l].offer.features;
-  popupTemplate.querySelector('p').textContent = ads[l].offer.description;
-  popupTemplate.querySelector('.popup__avatar').setAttribute('src', ads[l].author.avatar);
+var showMapCard = function(){
+  for (var l = 0; l < ads.length; l++) {
+  	return showMapCard;
+  }
 }
-var fragmentMap = document.createDocumentFragment();
-fragmentMap.appendChild(popupTemplate);
-mapCard.appendChild(fragmentMap);
+console.log(showMapCard());
+    var popupAdress = popupTemplate.querySelector('p:first-of-type');
+    popupTemplate.querySelector('h3').textContent = ads[l].offer.title;
+    popupAdress.querySelector('small').textContent = ads[l].offer.address;
+    popupTemplate.querySelector('.popup__price').textContent = ads[l].offer.price + '&#x20bd;/ночь';
+    popupTemplate.querySelector('h4').textContent = ads[l].offer.type;
+    popupTemplate.querySelector('h4 + p').textContent = ads[l].offer.rooms + ' для ' + ads[l].offer.guests + ' гостей';
+    popupTemplate.querySelector('p + p').textContent = 'Заезд после ' + ads[l].offer.checkin + ', выезд до ' + ads[l].offer.checkout;
+    popupTemplate.querySelector('.popup__features').textContent = ads[l].offer.features;
+    popupTemplate.querySelector('p').textContent = ads[l].offer.description;
+    popupTemplate.querySelector('.popup__avatar').setAttribute('src', ads[l].author.avatar);
+  var fragmentMap = document.createDocumentFragment();
+	fragmentMap.appendChild(popupTemplate);
+	mapCard.appendChild(fragmentMap);
+console.log(showMapCard());
