71674a93b73d92ced055087b1cac141000b4b05d
Unknown <stmihailov@yandex.ru>
2017-12-01 19:54:04 +0300

    правки

--- js/map.js
+++ js/map.js
@@ -7,16 +7,16 @@ var mapFadded = function () {
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
 };
-
+var ads = [];
 
 function generateRandomNumber(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 var renderAdsArray = function () {
-	var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира',
-  'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик',
-  'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+  var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик',
+    'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 
   var types = ['flat', 'house', 'bungalo'];
   var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -79,9 +79,7 @@ var showMapCard = function () {
   popupTemplate.querySelector('.popup__features').textContent = ads[0].offer.features;
   popupTemplate.querySelector('p').textContent = ads[0].offer.description;
   popupTemplate.querySelector('.popup__avatar').setAttribute('src', ads[0].author.avatar);
-  //for(var l = 0; l < ads[0].offer.features.length; l++) {
-    //var element = document.createElement(li);
-  //}
+
   var typeLive = popupTemplate.querySelector('h4').textContent;
   if (typeLive === 'house') {
     typeLive = 'Дом';
@@ -96,10 +94,9 @@ var showMapCard = function () {
   mapCard.appendChild(fragmentMap);
 };
 var initMap = function () {
-	var ads = [];
   mapFadded();
   ads = renderAdsArray();
-  showMapCard(ads[0]);
-  generateButton(ads[0]);
+  showMapCard();
+  generateButton();
 };
 initMap();
