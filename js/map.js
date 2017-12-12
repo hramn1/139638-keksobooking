6b852748be39e02056e6d28ae7c46abfe764be0f
hramn1 <stmihailov@yandex.ru>
2017-12-12 13:54:07 +0300

    правки

--- js/map.js
+++ js/map.js
@@ -1,7 +1,8 @@
 'use strict';
 
 window.map = (function () {
-
+  var MIN_HEIGHT_Y = 100;
+  var MAX_HEIGHT_Y = 500;
   var map = document.querySelector('.map');
   var ads = [];
   var mapActivate = document.querySelector('.map__pin--main');
@@ -47,32 +48,46 @@ window.map = (function () {
     return ads;
   };
 
-  var onMouseDown = function () {
-    mapActivate.addEventListener('mousedown', onMouseDown);
-    var onMouseMove = function (moveEvt) {
-      var cordsY = moveEvt.clientY;
-      var cordsX = moveEvt.clientX;
 
-      if (screen.width > 1200) {
-        cordsX = moveEvt.clientX - (screen.width - 1200) / 2;
-      } else {
-        cordsX = moveEvt.clientX;
-      }
+  var onMouseDown = function (event) {
+    event.preventDefault();
+    mapActivate.style.zIndex = '10';
+
+    var startCoords = {
+      x: event.clientX,
+      y: event.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var cordsY = mapActivate.offsetTop - shift.y;
+      var cordsX = mapActivate.offsetLeft - shift.x;
       mapActivate.style.left = cordsX + 'px';
 
-      if (moveEvt.clientY < 100) {
-        cordsY = 100;
-      } else if (moveEvt.clientY > 500) {
-        cordsY = 500;
+      if (mapActivate.offsetTop < MIN_HEIGHT_Y) {
+        cordsY = MIN_HEIGHT_Y;
+      } else if (mapActivate.offsetTop > MAX_HEIGHT_Y) {
+        cordsY = MAX_HEIGHT_Y;
       }
 
       mapActivate.style.top = cordsY + 'px';
       window.form.inputAdress.value = 'x: ' + cordsX + ' y: ' + cordsY;
     };
+
     var onMouseUp = function () {
       map.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
     };
+
     map.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
   };
@@ -81,7 +96,7 @@ window.map = (function () {
     mapActivate.removeEventListener('mouseup', window.pin.onButtonMouseup);
     mapFadded();
     ads = renderAdsArray();
-    onMouseDown();
+    mapActivate.addEventListener('mousedown', onMouseDown);
     window.pin.generateButton(ads);
   };
 
