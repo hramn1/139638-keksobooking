eb81aeec4597ca041e295e3e420e9c570437e663
Unknown <stmihailov@yandex.ru>
2017-12-15 02:01:38 +0300

    правки

---
+++ js/backend.js
@@ -0,0 +1,16 @@
+'use strict';
+
+window.backend = (function () {
+  var load = function () {
+    var xhr = new XMLHttpRequest();
+  };
+
+  var save = function () {
+  var xhr = new XMLHttpRequest();
+  };
+
+  return {
+    save: save,
+    load: load,
+  }
+})();
